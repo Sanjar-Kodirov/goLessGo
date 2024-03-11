19f874827d13124e0c8b4ed83ce07e7e20ca8176
sqodirov <sqodirov@cdti.uz>
2024-03-11 15:54:27 +0500

    add: divider

--- src/shared/ui/Divider/Divider.tsx
+++ src/shared/ui/Divider/Divider.tsx
@@ -1,5 +1,5 @@
 const Divider = () => {
-  return <div className="border-t mb-8 mt-8 w-[90%] mx-auto h-[100px]"></div>;
+  return <div className="border-t mb-8 mt-8 w-full mx-auto h-[100px]"></div>;
 };
 
 export default Divider;
